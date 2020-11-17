b0ca3b991f4c0ce7308c35042a70045afc1b77eb
github-action-benchmark <github@users.noreply.github.com>
2020-11-17 02:10:31 +0000

    add connect (cargo) benchmark result for 2f2ceb24265a7e63601cf1ffde7d586cd666a783

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1605579031396,
+  "repoUrl": "https://github.com/stevepentland/hyper",
+  "entries": {
+    "connect": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "2f2ceb24265a7e63601cf1ffde7d586cd666a783",
+          "message": "chore(ci): automatically publish benchmarks in a graph",
+          "timestamp": "2020-11-16T16:51:30-08:00",
+          "tree_id": "5b90f0b2e08fd62a41b6f1f90d45da63bd559713",
+          "url": "https://github.com/stevepentland/hyper/commit/2f2ceb24265a7e63601cf1ffde7d586cd666a783"
+        },
+        "date": 1605579026634,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 67532,
+            "range": "± 11738",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
