6be45923c04e34f20f3ff75449a100e007515e86
github-action-benchmark <github@users.noreply.github.com>
2020-11-17 02:10:41 +0000

    add pipeline (cargo) benchmark result for 2f2ceb24265a7e63601cf1ffde7d586cd666a783

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605579031396,
+  "lastUpdate": 1605579040572,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -29,6 +29,38 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 67532,
             "range": "± 11738",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "2f2ceb24265a7e63601cf1ffde7d586cd666a783",
+          "message": "chore(ci): automatically publish benchmarks in a graph",
+          "timestamp": "2020-11-16T16:51:30-08:00",
+          "tree_id": "5b90f0b2e08fd62a41b6f1f90d45da63bd559713",
+          "url": "https://github.com/stevepentland/hyper/commit/2f2ceb24265a7e63601cf1ffde7d586cd666a783"
+        },
+        "date": 1605579035918,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 68916,
+            "range": "± 24143",
             "unit": "ns/iter"
           }
         ]
