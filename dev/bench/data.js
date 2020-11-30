74996642c2df2a3b54ed5dce3da6994742954cce
github-action-benchmark <github@users.noreply.github.com>
2020-11-30 17:01:33 +0000

    add pipeline (cargo) benchmark result for 21dea2114574bbeda41bad5dff5e8e3613352124

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606544318519,
+  "lastUpdate": 1606755693115,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -601,6 +601,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 55346,
             "range": "± 4509",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "faern@faern.net",
+            "name": "Linus Färnstrand",
+            "username": "faern"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "21dea2114574bbeda41bad5dff5e8e3613352124",
+          "message": "chore(dependencies): upgrade socket2 (#2349)\n\nUpgrades to a version not making invalid assumptions about\nthe memory layout of std::net::SocketAddr",
+          "timestamp": "2020-11-30T05:48:28-08:00",
+          "tree_id": "9ab22d866d6f8bd5c19045971f7def89440f4b46",
+          "url": "https://github.com/stevepentland/hyper/commit/21dea2114574bbeda41bad5dff5e8e3613352124"
+        },
+        "date": 1606755689001,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 47001,
+            "range": "± 2324",
             "unit": "ns/iter"
           }
         ]
