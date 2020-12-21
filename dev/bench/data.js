bb49b792a79e8b7da669eb9f10045627df4066df
github-action-benchmark <github@users.noreply.github.com>
2020-12-21 22:45:17 +0000

    add connect (cargo) benchmark result for 6c593c292508f31a2986d5ddac3fb23b234a0e2c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608090462840,
+  "lastUpdate": 1608590716694,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -509,6 +509,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 49686,
             "range": "± 954",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "46450720+bishtpawan@users.noreply.github.com",
+            "name": "Pawan Singh Bisht",
+            "username": "bishtpawan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c593c292508f31a2986d5ddac3fb23b234a0e2c",
+          "message": "refactor(error): use `matches!` macro in `Error::is_*` methods (#2367)",
+          "timestamp": "2020-12-21T12:25:47-08:00",
+          "tree_id": "c61537b497a46b23e64414fa1f2c822dadd22fc4",
+          "url": "https://github.com/stevepentland/hyper/commit/6c593c292508f31a2986d5ddac3fb23b234a0e2c"
+        },
+        "date": 1608590712634,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 40736,
+            "range": "± 7047",
             "unit": "ns/iter"
           }
         ]
