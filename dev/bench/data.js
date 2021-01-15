243d973f95bb1ffa1bac3bc9c9858ad32006bba5
github-action-benchmark <github@users.noreply.github.com>
2021-01-15 21:32:21 +0000

    add pipeline (cargo) benchmark result for bf8d74ad1cf7d0b33b470b1e61625ebac56f9c4c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610669551287,
+  "lastUpdate": 1610746341414,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -1591,6 +1591,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 54282,
             "range": "± 3043",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "24809834+aeryz@users.noreply.github.com",
+            "name": "Abdullah Eryuzlu",
+            "username": "aeryz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf8d74ad1cf7d0b33b470b1e61625ebac56f9c4c",
+          "message": "feat(body): add `send_trailers` to Body channel's `Sender` (#2387)\n\nCloses #2260",
+          "timestamp": "2021-01-15T10:14:21-08:00",
+          "tree_id": "98ca623fc9171719aabfeac75bad0285557a25fb",
+          "url": "https://github.com/stevepentland/hyper/commit/bf8d74ad1cf7d0b33b470b1e61625ebac56f9c4c"
+        },
+        "date": 1610746337275,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 46810,
+            "range": "± 1779",
             "unit": "ns/iter"
           }
         ]
