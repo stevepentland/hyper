920d6b31a996aa0ff1a889a9a197eb054706020a
github-action-benchmark <github@users.noreply.github.com>
2021-03-05 14:08:21 +0000

    add connect (cargo) benchmark result for 8cbf9527dfb313b3f84fcd83260c5c72ce4a1beb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614406258209,
+  "lastUpdate": 1614953300665,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -1319,6 +1319,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 71637,
             "range": "± 9276",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter@smitmail.eu",
+            "name": "Peter Smit",
+            "username": "psmit"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8cbf9527dfb313b3f84fcd83260c5c72ce4a1beb",
+          "message": "fix(server): skip automatic Content-Length headers when not allowed (#2216)\n\nCloses #2215",
+          "timestamp": "2021-03-04T17:03:33-08:00",
+          "tree_id": "8c8f7d8ab9d86213ee1f48be464f206ede927d71",
+          "url": "https://github.com/stevepentland/hyper/commit/8cbf9527dfb313b3f84fcd83260c5c72ce4a1beb"
+        },
+        "date": 1614953296289,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 50016,
+            "range": "± 792",
             "unit": "ns/iter"
           }
         ]
