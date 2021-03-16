b894b77b4089967b322db24d992d0c86d0c4bb7e
github-action-benchmark <github@users.noreply.github.com>
2021-03-16 04:06:32 +0000

    add connect (cargo) benchmark result for eb0e7186963bbe4fa1ad4478ac493f75ddc92ae5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615441895943,
+  "lastUpdate": 1615867592079,
   "repoUrl": "https://github.com/stevepentland/hyper",
   "entries": {
     "connect": [
@@ -1409,6 +1409,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 52399,
             "range": "± 8731",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mail@dbrgn.ch",
+            "name": "Danilo Bargen",
+            "username": "dbrgn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eb0e7186963bbe4fa1ad4478ac493f75ddc92ae5",
+          "message": "docs(body): add links to to_bytes and aggregate (#2464)\n\nSince these two functions are not methods on the `Body`, they aren't\r\nvery discoverable. So a note in the docs would definitely be helpful.",
+          "timestamp": "2021-03-15T15:40:28-07:00",
+          "tree_id": "14b6039d4a111f52351c76ea2f9cdbb19cf616c6",
+          "url": "https://github.com/stevepentland/hyper/commit/eb0e7186963bbe4fa1ad4478ac493f75ddc92ae5"
+        },
+        "date": 1615867587897,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 42377,
+            "range": "± 5776",
             "unit": "ns/iter"
           }
         ]
